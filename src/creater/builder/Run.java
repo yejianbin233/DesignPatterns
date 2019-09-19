67a055137ce2171d23b86cb805bec2767c48db87
@yejianbin233 <877811992@qq.com>
2019-09-19 14:18:53 +0800

    builder run update

--- src/creater/builder/Run.java
+++ src/creater/builder/Run.java
@@ -15,7 +15,11 @@ public class Run {
         // 这里就相当于一个 Direction 类，指导如何创建一个机器人 -> 缺手缺脚的机器人
         Robot robot = new StandradRobotBuilder().buildHead().buildLeftFoot().buildLeftHand().build();
         // 也可以如书中，使用工厂方法来通过指令来命令 Direction 类创建各式各样的机器人
-
+        Direction direction = new Direction("standrad");
+        direction.order("head");
+        direction.order("leftHand");
+        Robot methodFactoryRobot = direction.build();
         System.out.println(robot);
+        System.out.println(methodFactoryRobot);
     }
 }
